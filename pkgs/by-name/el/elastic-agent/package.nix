902017c7a68d29430c73cadfa78f664cca0f766f
bstanderline <153822813+bstanderline@users.noreply.github.com>
2025-01-18 21:46:08 +0000

    Get build process working

--- pkgs/by-name/el/elastic-agent/package.nix
+++ pkgs/by-name/el/elastic-agent/package.nix
@@ -2,6 +2,9 @@
   lib,
   fetchFromGitHub,
   buildGoModule,
+  mage,
+  gotestsum,
+  writeShellScriptBin,
 }:
 
 buildGoModule rec {
@@ -15,7 +18,56 @@ buildGoModule rec {
     hash = "sha256-0y4321/BnkSLyYGUuZ79e9pJI0Fy5IhwPZzJpsYcyXo=";
   };
 
+  nativeBuildInputs =
+    let
+      fakeGit = writeShellScriptBin "git" ''
+        if [[ $@ = "rev-parse HEAD" ]]; then
+            echo "${version}"
+        else
+            >&2 echo "Unknown command: $@"
+            exit
+        fi
+      '';
+    in
+    [
+      fakeGit
+      mage
+    ];
+
+  nativeCheckInputs = [ gotestsum ];
+
   vendorHash = "sha256-pzAFiC4xSSgNsKjpXjQwxsfso6RZ9ULoPiujAqVXHcI=";
+
+  patches = [
+    ./gotest.patch
+  ];
+
+  ldflags = [
+    "-X main.Commit=${version}"
+    "-X main.version=${version}"
+    "-X main.builtBy=nixpkgs"
+  ];
+
+  buildPhase = ''
+    runHook prebuild
+
+    # Fixes "mkdir /homeless-shelter: permission denied" - "Error: error compiling magefiles" during build
+    export HOME=$(mktemp -d)
+    mage build:binaryOSS
+
+    runHook postBuild
+  '';
+
+  # Todo: Add tests...
+  checkPhase = ''
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    install -Dt $out/bin/elastic-agent-oss build/elastic-agent-oss
+    install -Dt $out/etc/elastic-agent.yml build/elastic-agent.yml
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "A single, unified way to add monitoring for logs, metrics, and other types of data to a host";
